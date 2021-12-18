e243a8b7eec35dc8e2ed19016720d3ef299d8df8
Alejahndro <alejahndro02@gmail.com>
2021-12-18 14:37:25 -0600

    se importo el modulo de AppRoutingmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 
 import { SharedModule } from '@shared/shared.module';
 import { PaisModule } from '@pais/pais.module';
@@ -12,8 +13,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     SharedModule,
-    PaisModule
+    PaisModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
