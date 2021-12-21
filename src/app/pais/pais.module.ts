276a2498557f84975e5f83b3c10d5949b6690134
Alejahndro <alejahndro02@gmail.com>
2021-12-21 12:15:21 -0600

    Se agrego el modulo de routingModule para poder usar la propiedad routerLink

--- src/app/pais/pais.module.ts
+++ src/app/pais/pais.module.ts
@@ -1,3 +1,4 @@
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
@@ -25,7 +26,8 @@ import { FormsModule } from '@angular/forms';
   ],
   imports: [
     CommonModule,
-    FormsModule
+    FormsModule,
+    RouterModule
   ]
 })
 export class PaisModule { }
