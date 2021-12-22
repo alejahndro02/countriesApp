8653d879f780f09b75632b002fd776150064d3e9
Alejahndro <alejahndro02@gmail.com>
2021-12-21 18:59:00 -0600

    Se crea un evento de Ondebounce y tambiens se declara un un osbservable mediante el operador subject  en el ciclo de vida OnInit se crea un pipe aue ejecutara la petiioin cada 300 ms que emitira el valor y por ultimo se declaro el metodo teclaPrecionadaaue ejecutara el evento next del subscribe

--- src/app/pais/components/pais-input/pais-input.component.ts
+++ src/app/pais/components/pais-input/pais-input.component.ts
@@ -1,4 +1,8 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
+
+
 
 @Component({
   selector: 'app-pais-input',
@@ -6,11 +10,34 @@ import { Component, EventEmitter, Output } from '@angular/core';
   styles: [
   ]
 })
-export class PaisInputComponent  {
-@Output() onEnter: EventEmitter<string> = new EventEmitter()
-  palabra:string=''
-buscar(){
-  console.log('pabra', this.palabra);
-  this.onEnter.emit(this.palabra)
+
+export class PaisInputComponent implements OnInit  {
+
+@Output() onEnter: EventEmitter<string> = new EventEmitter();
+//este evento se ejecuta cuando dejamos de teclear 
+@Output() onDebounce: EventEmitter<string> = new EventEmitter();
+
+//se crea un observable tipo Subject
+debouncer: Subject<string> = new Subject();
+
+palabra:string=''
+
+ngOnInit() {
+
+  this.debouncer.pipe(debounceTime(300)).subscribe(valor=>{
+    console.log('debouncer', valor);
+    this.onDebounce.emit(valor)
+    
+  })
 }
+
+buscar(){
+  console.log('palabra', this.palabra);
+  this.onEnter.emit(this.palabra)
+
+}
+teclaPrecionada(){
+  this.debouncer.next(this.palabra)
+}
+
 }
