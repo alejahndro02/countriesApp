3457856c3c79bb2f6e9a1dbdab6f4694f867f484
Alejahndro <alejahndro02@gmail.com>
2021-12-23 14:45:37 -0600

    Se implemento el operador de rxjs switchMap para optimizar el codigo de la peticion

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { PaisService } from '@pais/services/pais.service';
+import { switchMap } from 'rxjs/operators';
 @Component({
   selector: 'app-ver-pais',
   templateUrl: './ver-pais.component.html',
@@ -12,12 +13,20 @@ export class VerPaisComponent implements OnInit {
   constructor(private activateRoute: ActivatedRoute, private paisService: PaisService) { }
 
   ngOnInit(): void {
-    this.activateRoute.params.subscribe(({id}) =>{
-      console.log(id);
-      this.paisService.getPaisCode(id).subscribe(pais=>{
-        console.log(pais)
-      })
+    this.activateRoute.params
+    .pipe(
+      switchMap(({id})=> this.paisService.getPaisCode(id))
+    )
+    .subscribe(res => {
+      console.log(res);
+      
     })
+    // this.activateRoute.params.subscribe(({id}) =>{
+    //   console.log(id);
+    //   this.paisService.getPaisCode(id).subscribe(pais=>{
+    //     console.log(pais)
+    //   })
+    // })
   }
 
 }
