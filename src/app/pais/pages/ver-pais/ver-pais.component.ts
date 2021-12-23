21da5e247c36c558e6853e7a0f31b99faa99dde9
Alejahndro <alejahndro02@gmail.com>
2021-12-23 14:36:58 -0600

    Se inyecta la dependencia de activeRoute que estara observando si hay uin cambio en la url, tambien se inyecta el servio proveniente del archivo de pais.service

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute } from '@angular/router';
+import { PaisService } from '@pais/services/pais.service';
 @Component({
   selector: 'app-ver-pais',
   templateUrl: './ver-pais.component.html',
@@ -8,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VerPaisComponent implements OnInit {
 
-  constructor() { }
+  constructor(private activateRoute: ActivatedRoute, private paisService: PaisService) { }
 
   ngOnInit(): void {
+    this.activateRoute.params.subscribe(({id}) =>{
+      console.log(id);
+      this.paisService.getPaisCode(id).subscribe(pais=>{
+        console.log(pais)
+      })
+    })
   }
 
 }
