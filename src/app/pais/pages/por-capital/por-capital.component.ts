dcbb95b47b5eaeda559b5edd4e2469c1eaaf95af
Alejahndro <alejahndro02@gmail.com>
2021-12-23 14:09:37 -0600

    Se importaron la interface y el servicio, se creo el metodo buscar

--- src/app/pais/pages/por-capital/por-capital.component.ts
+++ src/app/pais/pages/por-capital/por-capital.component.ts
@@ -1,14 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { Country } from '@pais/interfaces/pais.interface';
+import { PaisService } from '@pais/services/pais.service';
 
 @Component({
   selector: 'app-por-capital',
   templateUrl: './por-capital.component.html',
 })
 export class PorCapitalComponent implements OnInit {
+  palabra:string = '';
+  hayError:boolean= false
+  paises:Country[]= [];
 
-  constructor() { }
+  constructor(private paiservice: PaisService) { }
+
 
   ngOnInit(): void {
   }
-
+  buscar(palabra:string){
+    this.hayError= false;
+    this.palabra = palabra
+    this.paiservice.buscarCapital(palabra)
+    .subscribe((paises) =>{
+      this.paises= [...paises]
+    }, (err)=>{
+      this.hayError=true
+      this.paises=[]
+    });
+  }
 }
