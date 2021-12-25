331556a975a422202909c99bf8ad3595d05e6d84
Alejahndro <alejahndro02@gmail.com>
2021-12-25 13:24:54 -0600

    Se declararon las propiedades badges y tranlationsLanguage, asi como los metodos iterarrBadgesKey y translationsLAnguage aue son ejecutadas dentro del ciclo de vida ngOnInit

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -11,19 +11,33 @@ import { switchMap, tap } from 'rxjs/operators';
 })
 export class VerPaisComponent implements OnInit {
   pais!: Country;
+  badges:string[]=[];
+  translationslanguages!: string[];
+
   constructor(private activateRoute: ActivatedRoute, private paisService: PaisService) { }
 
   ngOnInit(): void {
 
     this.activateRoute.params
     .pipe(
-      switchMap(({ id })=> this.paisService.getPaisCode( id )), 
-      // switchMap(( param )=> this.paisService.getPaisCode( param.id )), 
-
+      switchMap(({ id })=> this.paisService.getPaisCode( id )),
       tap(console.log)
     )
-    .subscribe( pais => this.pais = pais[0])
-
-  }
-
+    .subscribe( pais => {
+      this.pais = pais[0];
+      this.iterarBadgesKey();
+      // this.trasnslationLanguage();
+    });
+  };
+iterarBadgesKey(){
+      this.translationslanguages = Object.keys( this.pais.translations )
+      console.log('nenanena', this.translationslanguages);
+};
+trasnslationLanguage(){
+  const {translations} = this.pais;
+  const element = Object.values(translations);
+        for( let i =0; i < element.length; i++){
+        this.badges.push(element[i].common);
+      };
+};
 }
