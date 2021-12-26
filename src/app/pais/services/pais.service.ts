ddb3201ea4e1c394296f674ce33b7e82f2373cd7
Alejahndro <alejahndro02@gmail.com>
2021-12-26 13:26:33 -0600

    Se importo httpParams de angular/common para poder asignar parametros dentrode la url, creo un metodo tipo getter con las porpiedades de los parametros que complementaran la url

--- src/app/pais/services/pais.service.ts
+++ src/app/pais/services/pais.service.ts
@@ -1,28 +1,36 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Country } from '@pais/interfaces/pais.interface';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PaisService {
   private countrieUrl:string= environment.Url
+
+  get httpParams(){
+    return new HttpParams().set('fields', 'name,capital,cca2,flags,population')
+  }
   
   constructor(private http:HttpClient) { }
 
  buscarPais(palabra:string):Observable<Country[]>{
    const url = `${this.countrieUrl}/name/${palabra}`
-   return this.http.get<Country[]>(url);
+   return this.http.get<Country[]>(url,{params:this.httpParams});
   }
   buscarCapital(palabra:string):Observable<Country[]>{
     const url = `${this.countrieUrl}/capital/${palabra}`
-    return this.http.get<Country[]>(url);
+    return this.http.get<Country[]>(url,{params:this.httpParams});
   }
   buscarRegion(region:string):Observable<Country[]>{
     const url = `${this.countrieUrl}/region/${region}`
-    return this.http.get<Country[]>(url);
+    return this.http.get<Country[]>(url, {params:this.httpParams})
+    .pipe(
+      tap(console.log)
+    );
   }
   getPaisCode(id:string):Observable<Country>{
     const url = `${this.countrieUrl}/alpha/${id}`
