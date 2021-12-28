ba3d9fd06a807eb27c4a7be93366a1f40cfe153f
Alejahndro <alejahndro02@gmail.com>
2021-12-27 22:51:35 -0600

    Se declararon las propiedades paisesSugeridos, mostrarSugerencia y el metodo buscarSugerencia,dentro del metodo sugerencias se subscribe al servicio

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -6,19 +6,24 @@ import { PaisService } from '@services/pais.service';
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
   styles: [
+    `li{
+      cursor:pointer;
+    }`
   ]
 })
 export class PorPaisComponent  {
   palabra:string = '';
   hayError:boolean= false
   paises:Country[]= []
+  paisesSugeridos:Country[]= [];
+  mostrarSugerencia=false
 
-  constructor(private paiservice: PaisService) { }
+  constructor(private paisService: PaisService) { }
 
   buscar(palabra:string){
     this.hayError= false;
     this.palabra = palabra
-    this.paiservice.buscarPais(palabra)
+    this.paisService.buscarPais(palabra)
     .subscribe((paises) =>{
       this.paises= [...paises]
     }, (err)=>{
@@ -26,9 +31,21 @@ export class PorPaisComponent  {
       this.paises=[]
     });
   }
-  sugerencia(palabra:string){
-    console.log('algo');
+  sugerencias(palabra:string){
     this.hayError = false
-    // this.palabra = palabra
+    this.palabra = palabra
+    this.mostrarSugerencia = true
+
+    //obtiene los datos que coiciden desde la api atraves del servicio 
+    this.paisService.buscarPais(palabra)
+    .subscribe(paises =>{
+      this.paisesSugeridos= paises.slice(0,5)
+    },(err)=>{
+      this.paisesSugeridos=[]
+    })
+  }
+
+  buscarSugerencia(palabra:string){
+    this.buscar(palabra);
   }
 }
